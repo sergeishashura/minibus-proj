407d1a01e4b1c88e7d2a70e325bb7391d29197e7
sergejsasura <sergik031104@gmail.com>
2023-09-16 10:47:21 +0300

    new fields

---
+++ src/components/view_orders/Trip.js
@@ -0,0 +1,12 @@
+const Trip = () => {
+    return(
+        <div className="trip">
+            <span>Минск-Быхов         15:10       18:10     10р.    4 места</span>
+            <button>
+                Заказать
+            </button>
+        </div>
+    );
+}
+
+export default Trip;
